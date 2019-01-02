e369b16cc60818e5cb97681c47804963a703e55b
Ben <ben@wabow.com>
2019-01-02 11:39:39 +0800

    Bind Redis env to Container

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use Dotenv\Dotenv;
+use Predis\Client;
 
 $containerBuilder = new DI\ContainerBuilder;
 
@@ -9,6 +10,13 @@ $containerBuilder = new DI\ContainerBuilder;
  */
 $containerBuilder->addDefinitions([
     Dotenv::class => DI\autowire(Dotenv::class)->constructor(__DIR__),
+    Client::class => DI\autowire(Client::class)->constructor([
+        'scheme' => 'tcp',
+        'host' => DI\env('REDIS_HOST'),
+        'port' => DI\env('REDIS_PORT'),
+    ], [
+        'prefix' => DI\env('REDIS_PREFIX'),
+    ]),
 ]);
 
 $container = $containerBuilder->build();
