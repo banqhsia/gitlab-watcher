36f8f7c29b05baef672c6dd9bb95af5d0dacf331
Ben <ben@wabow.com>
2019-01-02 11:17:18 +0800

    Use ContainerBuilder instead of initialising Container
    Bind Dotenv to container

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,19 @@
 <?php
 
-(new Dotenv\Dotenv(__DIR__))->load();
+use Dotenv\Dotenv;
+
+$containerBuilder = new DI\ContainerBuilder;
+
+/**
+ * DI Container definitions
+ */
+$containerBuilder->addDefinitions([
+    Dotenv::class => DI\autowire(Dotenv::class)->constructor(__DIR__),
+]);
+
+$container = $containerBuilder->build();
+
+/**
+ * Bootstrap
+ */
+$container->get(Dotenv::class)->load();
--- index.php
+++ index.php
@@ -7,9 +7,6 @@ use App\Controller;
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/bootstrap.php';
 
-$container = new DI\Container;
-
-$controller = $container->get(Controller::class);
 $container->call(Controller::class . "::handle");
 
 exit;
